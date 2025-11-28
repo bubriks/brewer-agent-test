412cc48882631414e6e72050dde8d6f3eae175f9
bubriks <bubriks@gmail.com>
2025-11-28 14:30:48 +0200

    tmp

--- team.py
+++ team.py
@@ -12,9 +12,9 @@ from skills import add_to_team
 
 team = Team()
 
-test_p.add_to_team(team)
-test_1.add_to_team(team)
-test_2.add_to_team(team)
+#test_p.add_to_team(team)
+#test_1.add_to_team(team)
+#test_2.add_to_team(team)
 add_to_team(team)
 
 # Instead of get_agents we can just use the team and require the file to provide it
